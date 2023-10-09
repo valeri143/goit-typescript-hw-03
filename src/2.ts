4fada508703c04be56d526d3b01ff1900792f166
valeri143 <valeria23200523251605@gmail.com>
2023-10-09 21:35:16 +0300

    hw03

--- src/2.ts
+++ src/2.ts
@@ -16,9 +16,9 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  public name: string;
+  private department: string;
+  protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -32,6 +32,9 @@ class Employee {
 }
 
 class Manager extends Employee {
+  constructor(name: string, department: string, salary: number){
+    super(name, department, salary + 10000)
+  }
   // Реалізуйте конструктор та збільшіть salary на 10000
 }
 
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,46 @@
+class Key {
+    private signature: number;
+
+    constructor(){
+        this.signature = Math.random();
+    }
+
+    getSignature = ():number => {
+        return this.signature
+    }
+}
+
+class Person {
+    constructor(private key: Key){}
+     getKey = ():Key => {
+        return this.key
+    }
+}
+
+abstract class House {
+    protected door: boolean = false
+    protected key: Key
+    protected tenats:Person[] = []
+ constructor(key: Key) {
+    this.key = key;
+  }
+    comeIn = (person:Person): void => {
+        if(this.door){
+            this.tenats.push(person)
+        }
+    }
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+    openDoor = (key:Key):void =>{
+        if(key.getSignature() === this.key.getSignature()){
+            this.door = true
+        }
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
